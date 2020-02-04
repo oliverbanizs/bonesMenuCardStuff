206c319b83efb720082c92e3eb90f3dd1ee156e8
oliverbanizs <banizsoliver@gmail.com>
2020-02-04 11:31:22 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,39 @@
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class main extends Application implements EventHandler {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label mainDish = null;
+        Label sideOrder = null;
+        Label sauce = null;
+        Label dessert = null;
+        Label totalAmount = null;
+        Button mainAdd = null;
+        Button sideAdd = null;
+        Button sauceAdd = null;
+        Button dessertAdd = null;
+        Button summary = null;
+        TextField mainAm = null;
+        TextField sideAm = null;
+        TextField sauceAm = null;
+        TextField dessertAm = null;
+        TextField totalAm = null;
+        RadioButton med = null;
+        RadioButton large = null;
+        RadioButton xL = null;
+        
+
+    }
+
+    @Override
+    public void handle(Event event) {
+
+    }
+}
