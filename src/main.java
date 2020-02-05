01f6e26900b6e3925e48eb4d1d174124c4eb20c4
kovac <kovacsbela958@gmail.com>
2020-02-05 19:59:13 +0100

    át raktam pár cuccot

--- src/main.java
+++ src/main.java
@@ -5,29 +5,38 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class main extends Application implements EventHandler {
+
+    Label mainDish = null;
+    Label sideOrder = null;
+    Label sauce = null;
+    Label dessert = null;
+    Label totalAmount = null;
+    Button mainAdd = null;
+    Button sideAdd = null;
+    Button sauceAdd = null;
+    Button dessertAdd = null;
+    Button summary = null;
+    TextField mainAm = null;
+    TextField sideAm = null;
+    TextField sauceAm = null;
+    TextField dessertAm = null;
+    TextField totalAm = null;
+    RadioButton med = null;
+    RadioButton large = null;
+    RadioButton xL = null;
+    ImageView beckground = null;
+
+
+
+
     @Override
     public void start(Stage stage) throws Exception {
-        Label mainDish = null;
-        Label sideOrder = null;
-        Label sauce = null;
-        Label dessert = null;
-        Label totalAmount = null;
-        Button mainAdd = null;
-        Button sideAdd = null;
-        Button sauceAdd = null;
-        Button dessertAdd = null;
-        Button summary = null;
-        TextField mainAm = null;
-        TextField sideAm = null;
-        TextField sauceAm = null;
-        TextField dessertAm = null;
-        TextField totalAm = null;
-        RadioButton med = null;
-        RadioButton large = null;
-        RadioButton xL = null;
+
+
 
 
     }
